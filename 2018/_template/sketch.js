7662874a9360e188bd7bfe8a3bcc11717559889e
Alex Pendleton <alexcpendleton@gmail.com>
2018-11-01 18:34:21 -0700

    Add decent starting point

---
+++ 2018/_template/sketch.js
@@ -0,0 +1,88 @@
+let mid = {};
+let palette = [];
+let bg;
+function setup() {
+  //960,720;
+  createCanvas(screen.width, screen.height);
+  bg = qpColor("bg", "#fff");
+  background(bg);
+  mid = {
+    x: width / 2,
+    y: height / 2
+  };
+  //noLoop();
+  setupSeed();
+}
+
+function draw() {}
+
+function setupSeed() {
+  const fromLocation = new URL(document.location).searchParams.get("seed");
+  if (fromLocation) {
+    window.seed = parseFloat(fromLocation);
+  } else {
+    window.seed = ceil(random(0, 1000000));
+  }
+  console.log(window.seed);
+  randomSeed(window.seed);
+}
+
+function randomFromArray(arr) {
+  const i = Math.floor(random(0, arr.length));
+  return arr[i];
+}
+
+function qp(name, def, customParser) {
+  let value = new URL(document.location).searchParams.get(name);
+  if (value === null || value === undefined || value === "") {
+    value = def;
+  }
+  if (customParser) {
+    value = customParser(value);
+  } else {
+    if (!isNaN(value)) {
+      value = parseFloat(value);
+    }
+  }
+  console.log("qp", name, value, def);
+  return value;
+}
+function qpColor(name, def) {
+  return qp(name, def, v => {
+    if (v) {
+      return "#" + v;
+    }
+    return v;
+  });
+}
+
+function mouseReleased() {
+  if (mouseReleased.isCurrentlyPaused) {
+    console.log("resumed");
+    mouseReleased.isCurrentlyPaused = false;
+    return loop();
+  }
+  mouseReleased.isCurrentlyPaused = true;
+  noLoop();
+  console.log("paused");
+}
+
+function ca(c, a) {
+  const result = color(c);
+  if (a !== undefined) {
+    result.setAlpha(a);
+  }
+  return result;
+}
+
+function randomColorFromPalette() {
+  let x = randomFromArray(palette);
+  x = x.levels;
+  let r = color(x[0], x[1], x[2]);
+  return r;
+}
+
+function randomFromArray(arr) {
+  const i = Math.floor(random(0, arr.length));
+  return arr[i];
+}
